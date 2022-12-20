3655a63690f0360fb554c057e07589ff9570b738
Khoi Hoang <57012152+khoih-prog@users.noreply.github.com>
2022-12-20 14:05:17 -0500

    v1.2.0 for `ESP32_C3`
    #### Releases v1.2.0
    
    1. Add support to ESP32_C3-based boards using `LwIP W5500 Ethernet`

--- src/WebServer_ESP32_SC_W5500_Impl.h
+++ src/WebServer_ESP32_SC_W5500_Impl.h
@@ -1,9 +1,9 @@
 /****************************************************************************************************************************
   WebServer_ESP32_SC_W5500_Impl.h
 
-  For Ethernet shields using ESP32_SC_W5500 (ESP32_S2/3, ESP32_C3 + LwIP W5500)
+  For Ethernet shields using ESP32_SC_W5500 (ESP32_S2/S3/C3 + LwIP W5500)
 
-  WebServer_ESP32_SC_W5500 is a library for the ESP32_S2/3, ESP32_C3 with LwIP Ethernet W5500
+  WebServer_ESP32_SC_W5500 is a library for the ESP32_S2/S3/C3 with LwIP Ethernet W5500
 
   Based on and modified from ESP8266 https://github.com/esp8266/Arduino/releases
   Built by Khoi Hoang https://github.com/khoih-prog/WebServer_ESP32_SC_W5500
